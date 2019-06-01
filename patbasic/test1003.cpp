eccda5bad4eb7f0ee62a41f9c064cf3214da57d5
sunaowei <sunaowei@bytedance.com>
2019-06-01 12:57:34 +0800

    feat: test1003
    Change-Id: Ia8172c0cf5bc9c5a96884e4de83607102c30c04c

--- patbasic/test1003.cpp
+++ patbasic/test1003.cpp
@@ -6,52 +6,34 @@
 
 using namespace std;
 
-bool solution(const char *chars) {
-    int pos_P = -1;
-    int pos_T = -1;
 
-    int i = 0;
-    for (; chars[i] != '\0'; ++i) {
-        if (chars[i] == 'P') {
-            if (pos_P != -1) {
-                return false;
+bool isPass(string &s) {
+    int pIdx = -1, tIdx = -1;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'P') {
+            if (pIdx == -1) {
+                pIdx = i;
             } else {
-                pos_P = i;
-            }
-        } else if (chars[i] == 'A') {
-        } else if (chars[i] == 'T') {
-            if (pos_T != -1) {
                 return false;
-            } else {
-                pos_T = i;
             }
-        } else {
-            return false;
-        }
+        } else if (s[i] == 'T') {
+            if (tIdx == -1) {
+                tIdx = i;
+            } else {
+                return false;
+            }
+        } else if (s[i] != 'A') return false;
     }
-    int numA_mid = pos_T - pos_P - 1;
-    int numA_left = pos_P;
-    int numA_right = i - pos_T - 1;
-    if (numA_mid < 1) {
-        return false;
-    }
-    if (numA_left == 0 && numA_right == 0) {
-        return true;
-    } else if (numA_left == 0 || numA_right == 0) {
-        cout << "asdasdsadasdasddsa" << endl;
-        return false;
-    }
-    return (numA_right - numA_left) / numA_left == numA_mid - 1;
-
+    return (tIdx - pIdx - 1) > 0 && (tIdx - pIdx - 1) * pIdx == s.size() - tIdx - 1;
 }
 
-int test1003() {
+int main() {
     int n;
     cin >> n;
-    char chars[100] = {0};
-    for (int i = 0; i < n; ++i) {
-        cin >> chars;
-        cout << (solution(chars) ? "YES" : "NO") << endl;
+    while (n--) {
+        string s;
+        cin >> s;
+        cout << (isPass(s) ? "YES" : "NO") << endl;
     }
     return 0;
 }
